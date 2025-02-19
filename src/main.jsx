5de1125f861717be3320cca228913995ad647965
Mihael <mihael.pavlakovic@gmail.com>
2025-02-19 18:16:18 +0100

    chore: update base path

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ConfigProvider } from "antd";
+import "./styles.css";
 
 const themeConfig = {
   token: {
